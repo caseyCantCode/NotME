727cbc7541f4295c3f71ab36e9eeddd92b3e569d
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2021-10-23 10:56:12 +0700

    trying to fix bot's problems part 7

--- commands/utils/chatbot.js
+++ commands/utils/chatbot.js
@@ -15,7 +15,7 @@ module.exports = class Command extends Commando.Command {
 				{
 					key: 'channel',
 					prompt: 'Which text channel would you like to set my chatbot feature to?',
-					type: 'channel',
+					type: 'text-channel',
 					default: null,
 				},
 			],
@@ -30,7 +30,7 @@ module.exports = class Command extends Commando.Command {
 			return message.channel.send(`${message.client.emotes.success} - Chatbot feature has been turned off.`);
 		}
 
-		db.set(`${message.guild.id}.chatbotChannel`, `${channel}`);
+		db.set(`${message.guild.id}.chatbotChannel`, `${channel.id}`);
 
 		return message.channel.send(`${message.client.emotes.success} - Successfully set the chatbot channel to ${channel.toString()}!`);
 	}
