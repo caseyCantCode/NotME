dcd3ee2f485a82a8c997a3790d1b76463cbdf7f5
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2021-10-24 17:11:47 +0700

    added more games prt 8

---
+++ commands/games/button_press.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const Commando = require('discord.js-commando');
+
+module.exports = class Command extends Commando.Command {
+	constructor(client) {
+		super(client, {
+			name: 'will-you-press-the-button',
+			aliases: ['wyptb', 'button-press'],
+			group: 'games',
+			memberName: 'will-you-press-the-button',
+			ownerOnly: false,
+			guildOnly: true,
+			description: 'Will you press the button?',
+		});
+	}
+
+	async run(message) {
+		await message.client.weky.WillYouPressTheButton({
+			message: message,
+			embed: {
+				title: 'Will you press the button?',
+				description: '```{{statement1}}```\n**but**\n\n```{{statement2}}```',
+				color: message.client.config.discord.accentColor,
+				footer: 'just a game',
+				timestamp: true,
+			},
+			button: { yes: 'Yes', no: 'No' },
+			thinkMessage: "I'm thinking",
+			othersMessage: 'Only <@{{author}}> can use the buttons!',
+		});
+	}
+};
