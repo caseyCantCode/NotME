aa30be8d5e98a72089396a8f513da952021017f0
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2021-10-25 22:53:57 +0700

    added profiles and leveling system

---
+++ commands/info/profile.js
@@ -0,0 +1,85 @@
+const { MessageAttachment } = require('discord.js');
+const Commando = require('discord.js-commando');
+const { Canvas } = require('canvas-constructor');
+const { resolve, join } = require('path');
+const fetch = require('node-fetch');
+
+Canvas.registerFont(resolve(join(__dirname, './discord.otf')), 'Discord');
+
+const imageUrlRegex = /\?size=2048$/g;
+
+async function profile(member, key) {
+	const { level, points } = client.points.get(key);
+
+	try {
+		const result = await fetch(member.user.displayAvatarURL.replace(imageUrlRegex, '?size=128'));
+		if (!result.ok) throw new Error('Failed to get the avatar.');
+		const avatar = await result.buffer();
+
+		const name = member.displayName.length > 20 ? member.displayName.substring(0, 17) + '...' : member.displayName;
+
+		return new Canvas(400, 180)
+			.setColor('#7289DA')
+			.printRectangle(84, 0, 316, 180)
+			.setColor('#2C2F33')
+			.printRectangle(0, 0, 84, 180)
+			.printRectangle(169, 26, 231, 46)
+			.printRectangle(224, 108, 176, 46)
+			.setShadowColor('rgba(22, 22, 22, 1)')
+			.setShadowOffsetY(5)
+			.setShadowBlur(10)
+			.printCircle(84, 90, 62)
+			.printCircularImage(avatar, 20, 26, 64)
+			.save()
+			.createRoundedClip(20, 138, 128, 32, 5)
+			.setColor('#23272A')
+			.fill()
+			.restore()
+			.setTextAlign('center')
+			.setTextFont('10pt Discord')
+			.setColor('#FFFFFF')
+			.printText(name, 285, 54)
+			.printText(`Level: ${level.toLocaleString()}`, 84, 159)
+			.setTextAlign('left')
+			.printText(`Score: ${points.toLocaleString()}`, 241, 136);
+	} catch (error) {
+		message.channel.send(`Something happened: ${error.message}`);
+	}
+}
+
+module.exports = class Command extends Commando.Command {
+	constructor(client) {
+		super(client, {
+			name: 'profile',
+			group: 'info',
+			memberName: 'profile',
+			ownerOnly: false,
+			guildOnly: false,
+			description: 'Shows your profile.',
+			args: [
+				{
+					key: 'user',
+					prompt: 'What user do you want to use with?',
+					type: 'member',
+				},
+			],
+		});
+	}
+
+	async run(message, { user }) {
+		const key = `${message.guild.id}-${user.user.id}`;
+
+		message.client.points.ensure(`${message.guild.id}-${user.user.id}`, {
+			user: user.user.id,
+			guild: message.guild.id,
+			points: 0,
+			level: 1,
+		});
+
+		const buffer = await profile(user, client.points.get(key));
+		const filename = `profile-${user.user.id}.jpg`;
+		const attachment = new MessageAttachment(buffer, filename);
+
+		message.channel.send(attachment);
+	}
+};
