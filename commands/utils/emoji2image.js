b98bc2b0b2c27e746f03c3bfddb6503c60655b0c
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2021-10-25 12:28:23 +0700

    Uncommited Changes

--- commands/utils/emoji2image.js
+++ commands/utils/emoji2image.js
@@ -14,7 +14,7 @@ module.exports = class Command extends Commando.Command {
 			args: [
 				{
 					key: 'emoji',
-					prompt: 'Which **custom emoji** do you want to use?',
+					prompt: 'Which **custom emoji** do you want to use with?',
 					type: 'custom-emoji|default-emoji',
 				},
 			],
