6d1722c29a5f18c5d06243e7d52704c4642e57a5
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2021-10-26 08:17:44 +0700

    added canvas 8

--- index.js
+++ index.js
@@ -273,6 +273,26 @@ client.points = new Enmap('points');
 client.on('message', async (message) => {
 	if (message.author.bot) return;
 
+	if (message.guild && message.guild.id !== '739811956638220298') {
+		const key = `${message.guild.id}-${message.author.id}`;
+
+		client.points.ensure(`${message.guild.id}-${message.author.id}`, {
+			user: message.author.id,
+			guild: message.guild.id,
+			points: 0,
+			level: 1,
+		});
+
+		client.points.inc(key, 'points');
+
+		const curLevel = Math.floor(0.1 * Math.sqrt(client.points.get(key, 'points')));
+
+		if (client.points.get(key, 'level') < curLevel) {
+			message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
+			client.points.set(key, curLevel, 'level');
+		}
+	}
+
 	if (!db.has(`${message.guild.id}`)) {
 		db.set(`${message.guild.id}`, {});
 	}
@@ -304,26 +324,6 @@ client.on('message', async (message) => {
 				});
 		}
 	} else {
-		if (message.guild && message.guild.id !== '739811956638220298') {
-			const key = `${message.guild.id}-${message.author.id}`;
-
-			client.points.ensure(`${message.guild.id}-${message.author.id}`, {
-				user: message.author.id,
-				guild: message.guild.id,
-				points: 0,
-				level: 1,
-			});
-
-			client.points.inc(key, 'points');
-
-			const curLevel = Math.floor(0.1 * Math.sqrt(client.points.get(key, 'points')));
-
-			if (client.points.get(key, 'level') < curLevel) {
-				message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
-				client.points.set(key, curLevel, 'level');
-			}
-		}
-
 		if (message.content == '' || message.content.includes('hmm')) return;
 
 		let channel;
