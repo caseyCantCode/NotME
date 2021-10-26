6ebee774efe232483f1777310a4b806f752782e2
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2021-10-26 07:33:39 +0700

    added canvas 5

--- commands/info/profile.js
+++ commands/info/profile.js
@@ -4,7 +4,7 @@ const { Canvas, registerFont } = require('canvas-constructor/cairo');
 const { resolve, join } = require('path');
 const fetch = require('node-fetch');
 
-registerFont(resolve(join(__dirname, './discord.otf')), 'Discord');
+registerFont(resolve(join(__dirname, '../../discord.otf')), 'Discord');
 
 const imageUrlRegex = /\?size=2048$/g;
 
