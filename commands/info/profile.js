5f4a24f17d5c0229fa7d5aed22bae1b52675d46c
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2021-10-26 19:16:26 +0700

    test 12

--- commands/info/profile.js
+++ commands/info/profile.js
@@ -48,7 +48,7 @@ module.exports = class Command extends Commando.Command {
 
 				if (name.match(/[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/g)) {
 					let translated = await translate(member.user.username, { from: 'ja', to: 'en' });
-					name = translated;
+					name = translated + member.user.discriminator;
 				}
 
 				return new Canvas(400, 180)
