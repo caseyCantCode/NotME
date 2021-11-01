19459688c29a185e18eb91c739fe189f0e74bb54
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2021-11-01 19:33:43 +0700

    aaa 2

--- commands/levelling/leaderboard.js
+++ commands/levelling/leaderboard.js
@@ -18,7 +18,7 @@ module.exports = class Command extends Commando.Command {
 		const filtered = message.client.points.filter((p) => p.guild === message.guild.id).array();
 		const sorted = filtered.sort((a, b) => b.points - a.points);
 		const top10 = sorted.splice(0, 10);
-		const embed = new MessageEmbed().setTitle('Ranking Leaderboard').setAuthor(message.guild.name, message.guild.iconURL()).setColor(message.client.config.discord.accentColor);
+		const embed = new MessageEmbed().setTitle('Ranking Leaderboard').setAuthor(`${message.guild.name}'s'`, message.guild.iconURL()).setColor(message.client.config.discord.accentColor);
 		for (const data of top10) {
 			try {
 				embed.addField(message.client.users.cache.get(data.user).tag, `${data.points} XP (level ${data.level})`);
